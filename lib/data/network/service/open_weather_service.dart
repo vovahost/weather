9374b91095e9d80e2b736ec70a2fdfb49cd7c369
Vladimir Ostaci <vlos@nodesagency.com>
2023-08-11 09:23:10 +0100

    Add Open Weather service

---
+++ lib/data/network/service/open_weather_service.dart
@@ -0,0 +1,18 @@
+import 'package:weather/data/network/api_helper.dart';
+
+class OpenWeatherService {
+  static const String _baseUrl = "https://api.openweathermap.org/data/2.5";
+  static const String _openWeatherApiKey = "c2244bfdf7f3262c530985717be5974e";
+
+  final _apiHelper = ApiHelper();
+
+  Future<dynamic> getCityWeather({
+    required String city,
+    required String unit,
+  }) async {
+    final query = "q=$city&appid=$_openWeatherApiKey&units=$unit";
+    final path = "$_baseUrl/weather?$query";
+    final response = await _apiHelper.get(path);
+    return response;
+  }
+}
