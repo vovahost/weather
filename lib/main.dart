bf6ff0d1e6b54c40c2ba84f201753219f99d590d
Vladimir Ostaci <vlos@nodesagency.com>
2023-08-11 05:55:00 +0100

    Improve naming

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:weather/presentation/ui/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const WeatherApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Weather App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(title: 'Weather'),
     );
   }
 }
